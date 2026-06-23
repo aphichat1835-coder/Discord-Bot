ea17faf2579d5f0b120efc07730542bafe65e354
aphichat1835-coder <aphichat1835@gmail.com>
2026-06-23 23:36:46 +0700

    Use replaceAll in audit dashboard escaping

--- discord/index/auditDashboardPage.js
+++ discord/index/auditDashboardPage.js
@@ -1,10 +1,10 @@
 function escapeHtml(value) {
     return String(value ?? "")
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#39;");
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("\"", "&quot;")
+        .replaceAll("'", "&#39;");
 }
 
 function buildAuditDashboardPage({ title = "Audit Logs" } = {}) {
