4cdad4e70647fcf2e454a27211fe2406fc70680e
aphichat1835-coder <aphichat1835@gmail.com>
2026-06-23 14:34:58 +0700

    Add audit web bundle registration helper

---
+++ discord/index/auditWebBundle.js
@@ -0,0 +1,16 @@
+const { registerAuditApiRoutes } = require("./auditApiRoutes");
+const { buildAuditDashboardPage } = require("./auditDashboardPage");
+
+function registerAuditWebBundle({ app, express, sessionManager, client, auditLogger, checkAuth }) {
+    registerAuditApiRoutes({ app, express, sessionManager, client, auditLogger, checkAuth });
+
+    app.get("/audit-logs", (req, res) => {
+        if (!checkAuth(req, res)) return;
+        res.setHeader("Content-Type", "text/html; charset=utf-8");
+        res.send(buildAuditDashboardPage());
+    });
+}
+
+module.exports = {
+    registerAuditWebBundle
+};
