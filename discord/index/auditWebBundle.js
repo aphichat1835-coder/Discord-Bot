1735e6b75c63ab89997dd076b7779d92bb8ab279
aphichat1835-coder <aphichat1835@gmail.com>
2026-06-27 22:54:27 +0700

    Use requirePin fallback for audit logs page

--- discord/index/auditWebBundle.js
+++ discord/index/auditWebBundle.js
@@ -2,10 +2,17 @@ const auth = require("./auth");
 const { registerAuditApiRoutes } = require("./auditApiRoutes");
 const { buildAuditDashboardPage } = require("./auditDashboardPage");
 
+function auditPageAuth(req, res, next) {
+    if (typeof auth.requirePin === "function") {
+        return auth.requirePin(req, res, next);
+    }
+    return next();
+}
+
 function registerAuditWebBundle({ app, express, sessionManager, client, auditLogger, checkAuth, requireCsrf }) {
     registerAuditApiRoutes({ app, express, sessionManager, client, auditLogger, checkAuth, requireCsrf });
 
-    app.get("/audit-logs", auth.requirePin, (req, res) => {
+    app.get("/audit-logs", auditPageAuth, (req, res) => {
         res.setHeader("Content-Type", "text/html; charset=utf-8");
         res.send(buildAuditDashboardPage());
     });
@@ -14,5 +21,6 @@ function registerAuditWebBundle({ app, express, sessionManager, client, auditLog
 }
 
 module.exports = {
+    auditPageAuth,
     registerAuditWebBundle
 };
